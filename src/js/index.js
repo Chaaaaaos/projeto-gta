7b3a079205ed80890ed0567f50d53ce807a4ecec
Lucas Tomazini <lucasgtomazini@gmail.com>
2024-07-12 11:24:32 -0300

    added the README, resolved the responsiveness problem and a problem when selecting the platform that closed the session instead of clicking the button.

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,16 @@
 const botao = document.querySelector(".btn-plataforma");
 const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
+const botoesPlataformas = document.querySelectorAll(".btn-plataforma .plataformas li");
 
 botao.addEventListener("click", () => {
     elementoPlataformas.classList.toggle("ativo");
+    event.stopPropagation();
+});
+
+
+botoesPlataformas.forEach((botaoPlataforma) => {
+    botaoPlataforma.addEventListener("click", (event) => {
+        event.stopPropagation();
+        console.log("Plataforma selecionada:", botaoPlataforma.alt);
+    });
 });
